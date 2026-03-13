60bf34dd67b38439561f5863235dd0c1e45fc5be
ggm77 <shmshm86@naver.com>
2026-03-13 23:50:01 +0900

    feat: add add server startup example

--- src/main/java/com/seohamin/jastapi_example/Main.java
+++ src/main/java/com/seohamin/jastapi_example/Main.java
@@ -1,4 +1,10 @@
 package com.seohamin.jastapi_example;
 
+import com.seohamin.jastapi.JastApiApplication;
+
 public class Main {
+
+    public static void main(String[] args) {
+        JastApiApplication.run(true, 8080);
+    }
 }
