75c1f5fad4ee21e1ebd5e6b957db4a4ced7acf65
ggm77 <shmshm86@naver.com>
2026-03-15 22:19:59 +0900

    feat: add HttpStatus enum

---
+++ src/main/java/com/seohamin/jastapi/web/http/HttpStatus.java
@@ -0,0 +1,46 @@
+package com.seohamin.jastapi.web.http;
+
+public enum HttpStatus {
+
+    // 200
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    ACCEPTED(202, "Accepted"),
+    NO_CONTENT(204, "No Content"),
+    PARTIAL_CONTENT(206, "Partial Content"),
+
+    // 300
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    NOT_MODIFIED(304, "Not Modified"),
+
+    // 400
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    REQUEST_TIMEOUT(408, "Request Timeout"),
+    CONFLICT(409, "Conflict"),
+    PAYLOAD_TOO_LARGE(413, "Payload Too Large"),
+    RANGE_NOT_SATISFIABLE(416, "Range Not Satisfiable"),
+    REQUEST_HEADER_FIELDS_TOO_LARGE(431, "Request Header Fields Too Large"),
+
+    // 500
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    NOT_IMPLEMENTED(501, "Not Implemented"),
+    BAD_GATEWAY(502, "Bad Gateway"),
+    SERVICE_UNAVAILABLE(503, "Service Unavailable"),
+    GATEWAY_TIMEOUT(504, "Gateway Timeout");
+
+    HttpStatus(
+            final int statusCode,
+            final String statusMessage
+    ) {
+        this.statusCode = statusCode;
+        this.statusMessage = statusMessage;
+    }
+
+    private int statusCode;
+    private String statusMessage;
+}
