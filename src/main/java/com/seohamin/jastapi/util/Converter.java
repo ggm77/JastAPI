0cbb399f85bfcfb91971e103a9db4dbf2f33ecae
ggm77 <shmshm86@naver.com>
2026-03-30 22:35:10 +0900

    feat: add Jackson library for Object-to-JSON serialization

--- src/main/java/com/seohamin/jastapi/util/Converter.java
+++ src/main/java/com/seohamin/jastapi/util/Converter.java
@@ -1,8 +1,12 @@
 package com.seohamin.jastapi.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
 
 public class Converter {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     // 인스턴스화 방지
     private Converter() {}
@@ -25,7 +29,12 @@ public class Converter {
             return ((String) object).getBytes(StandardCharsets.UTF_8);
         }
         else {
-            return object.toString().getBytes(StandardCharsets.UTF_8);
+            try {
+                return objectMapper.writeValueAsBytes(object);
+            } catch (JsonProcessingException ex) {
+                System.err.println("[ERROR] JSON Processing Exception occur");
+                return new byte[0];
+            }
         }
     }
 }
