d9fc0e15150e40852ea2055793853afad8021707
ggm77 <shmshm86@naver.com>
2026-04-15 22:23:41 +0900

    feat: add controller to serve index.html

---
+++ src/main/java/com/seohamin/jastapi_example/home/HomeController.java
@@ -0,0 +1,50 @@
+package com.seohamin.jastapi_example.home;
+
+import com.seohamin.jastapi.annotation.Component;
+import com.seohamin.jastapi.annotation.Get;
+import com.seohamin.jastapi.util.ErrorResponse;
+import com.seohamin.jastapi.util.HttpTime;
+import com.seohamin.jastapi.web.http.HttpHeader;
+import com.seohamin.jastapi.web.http.HttpResponse;
+import com.seohamin.jastapi.web.http.HttpStatus;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class HomeController {
+
+    @Get("/")
+    public HttpResponse getHomePage() {
+
+        byte[] body;
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("index.html")) {
+            if (is != null) {
+                body = is.readAllBytes();
+            } else {
+                body = new byte[0];
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return ErrorResponse.createNotFound("HTTP/1.1");
+        }
+
+        final HttpHeader responseHeader = new HttpHeader();
+        responseHeader.add("Content-Type", "text/html; charset=utf-8");
+        responseHeader.add("Content-Length", String.valueOf(body.length));
+        responseHeader.add("Connection", "keep-alive");
+        responseHeader.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        responseHeader.add("Date", HttpTime.getCurrentTime());
+
+
+        // http response 객체 필드 채우기
+        final HttpResponse httpResponse = new HttpResponse();
+        httpResponse.setStatusCode(HttpStatus.OK.getStatusCode());
+        httpResponse.setStatusMessage(HttpStatus.OK.getStatusMessage());
+        httpResponse.setVersion("HTTP/1.1");
+        httpResponse.setBody(body);
+        httpResponse.setHeader(responseHeader);
+
+        return httpResponse;
+    }
+}
